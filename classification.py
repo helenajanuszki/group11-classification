270a0b61a8d7d9181cc8f58b40e4ad0b7eef993a
Helena Januszki <helena.januszki@gmail.com>
2026-03-11 19:29:27 -0400

    scraper

--- classification.py
+++ classification.py
@@ -1,45 +1,71 @@
-import pandas as pd 
-
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
-
+import pandas as pd
+from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+from google_play_scraper import reviews
 
+
+#part 1
 df = pd.read_csv("tse_dataset.csv")
+
 df = df[df["category"].isin(["FEATURE", "BUG"])]
 
 X = df["body"]
-
 y = df["category"]
 
-vectorizer = TfidfVectorizer(stop_words="english")
-X_vectorized = vectorizer.fit_transform(X)
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X_vectorized, y, test_size=0.2, random_state=42
-)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-models = {
-    "Random Forest": RandomForestClassifier(),
-    "KNN": KNeighborsClassifier(),
-    "Decision Tree": DecisionTreeClassifier()
+svm = SVC()
+svm.fit(X_train, y_train)
+svm_predict = svm.predict(X_test)
+
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+knn_predict = knn.predict(X_test)
+
+rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
+rf_predict = rf.predict(X_test)
+
+model_dict = {
+    "Support Vector Machine": svm_predict,
+    "K Nearest Neighbors": knn_predict,
+    "Random Forest": rf_predict
 }
 
-for name, model in models.items():
+for model, prediction in model_dict.items():
+    print({model})
 
-    model.fit(X_train, y_train)
+    print("\nConfusion Matrix")
+    cm = confusion_matrix(y_test, prediction)
+    print(pd.DataFrame(cm, index=["Actual Bug","Actual Feature"], columns=["Predicted as a Bug","Predicted as a Feature"]))
 
-    predictions = model.predict(X_test)
+    print("\nResults")
+    print(classification_report(y_test, prediction))
 
-    print(name)
+#part 2
 
-    print("Accuracy:", accuracy_score(y_test, predictions))
-    print("Precision:", precision_score(y_test, predictions, average="weighted"))
-    print("Recall:", recall_score(y_test, predictions, average="weighted"))
-    print("F1 Score:", f1_score(y_test, predictions, average="weighted"))
+zoom, cont_token = reviews(
+    'us.zoom.videomeetings',
+    lang='en',
+    country='us',
+)
+zoom_df = pd.DataFrame(zoom)
+zoom_df = zoom_df[["userName", "content", "score", "at"]]
 
-    print("Confusion Matrix:")
-    print(confusion_matrix(y_test, predictions))
+# Filter by latest 2 months
+zoom_df['at'] = pd.to_datetime(zoom_df['at'])
+df_reviews = zoom_df[zoom_df['at'].dt.month >= 2]
+
+X_zoom = zoom_df["content"]
+X_zoom = vectorizer.transform(X_zoom)
+
+zoom_predictions = knn.predict(X_zoom)
+zoom_df["category"] = zoom_predictions
+zoom_df.to_csv("classified_reviews.csv")
